d956d3e9b29a0f52ffe39f7c368cbb3f07ccbe1b
Tushar_github <mahmudurrahman23yahoo@gmail.com>
2026-01-07 21:08:02 +0600

    Update urls.py

--- mail/urls.py
+++ mail/urls.py
@@ -1,14 +1,10 @@
+# mail/urls.py
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
-
-    # API Routes
-    path("emails", views.compose, name="compose"),                    # POST to send
-    path("emails/<int:email_id>", views.email, name="email"),         # GET/PUT/DELETE single email
-    path("emails/<str:mailbox>", views.mailbox, name="mailbox"),      # GET mailbox list
+    path("", views.index, name="mail_index"),
+    path("emails/<str:mailbox_name>", views.mailbox, name="mailbox"),
+    path("emails/<int:email_id>", views.email_detail, name="email_detail"),
+    path("emails/<int:email_id>/action", views.email_action, name="email_action"),
 ]
